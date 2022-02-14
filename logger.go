b5c3fb8d13f7ea5656e7803ed87516f901bafd0c
Edouard Hur <edouard.hur@iguanesolutions.com>
2022-02-14 20:03:25 +0100

    make it opinionated removed config, auto config with systemd detection

--- logger.go
+++ logger.go
@@ -3,38 +3,32 @@ package hllogger
 import (
 	"io"
 	"log"
+	"os"
 )
 
-// Forwarders to the log package flags
-const (
-	Ldate         = log.Ldate
-	Ltime         = log.Ltime
-	Lmicroseconds = log.Lmicroseconds
-	Llongfile     = log.Llongfile
-	Lshortfile    = log.Lshortfile
-	LUTC          = log.LUTC
-	LstdFlags     = log.LstdFlags
-)
+// New is the constructor for a HlLogger object
+func New(output io.Writer, level LogLevel) *HlLogger {
+	var (
+		journaldPrefix bool
+		flags          int
+	)
+	// Is systemd detected ?
+	_, journaldPrefix = os.LookupEnv("INVOCATION_ID")
+	if !journaldPrefix {
+		// systemd not detected, add date and time as prefix for logging
+		flags = log.Ltime | log.Ldate
+	}
+	// Return the initialized logger
+	return &HlLogger{
+		journald: journaldPrefix,
+		llevel:   level,
+		logger:   log.New(output, "", flags),
+	}
+}
 
 // HlLogger is a wrapper around the log/logger
 type HlLogger struct {
 	journald bool
 	llevel   LogLevel
 	logger   *log.Logger
-}
-
-// Config allows to configure an hllogger
-type Config struct {
-	LogLevel       LogLevel
-	LoggerFlags    int
-	JournaldPrefix bool
-}
-
-// New is the constructor for a HlLogger object
-func New(output io.Writer, c Config) *HlLogger {
-	return &HlLogger{
-		journald: c.JournaldPrefix,
-		llevel:   c.LogLevel,
-		logger:   log.New(output, "", c.LoggerFlags),
-	}
 }
